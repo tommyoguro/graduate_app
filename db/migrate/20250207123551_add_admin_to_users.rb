b38f7b6bce8c967f6e1a2bd44dc5ab47ed68ed32
tommyoguro <tomoharu.oguro.2022@globis.ac.jp>
2025-02-08 18:35:06 +0900

    init

--- db/migrate/20250207123551_add_admin_to_users.rb
+++ db/migrate/20250207123551_add_admin_to_users.rb
@@ -1,6 +1,7 @@
 class AddAdminToUsers < ActiveRecord::Migration[7.2]
   def change
-    add_column :users, :admin, :boolean, default: false, null: false
+    unless column_exists?(:users, :admin) # 既に存在する場合は追加しない
+      add_column :users, :admin, :boolean, default: false, null: false
+    end
   end
 end
-
