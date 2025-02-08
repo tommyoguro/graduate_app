28329739d80de8259f762da8d28834ad604d9ff0
tommyoguro <tomoharu.oguro.2022@globis.ac.jp>
2025-02-08 11:00:04 +0900

    init

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,7 +7,32 @@ class Users::SessionsController < Devise::SessionsController
    def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
+ 
+   def guest_sign_in
+     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+       user.password = SecureRandom.urlsafe_base64
+       user.name = "ゲストユーザー"
+       user.name_kana = "ゲスト"
+       user.company_name = "ゲスト会社"
+       user.division_name = "ゲスト部門"
+   end
+    sign_in user
+    redirect_to menu_path, notice: "ゲストユーザーとしてログインしました。"
+   end
 
+  def admin_guest_sign_in
+    admin = User.find_or_create_by!(email: 'admin_guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト管理者"
+      user.name_kana = "ゲスト管理"
+      user.company_name = "管理会社"
+      user.division_name = "管理部門"
+      user.admin = true # 管理者フラグを true に設定
+    end
+    sign_in admin
+    redirect_to admin_users_path, notice: "ゲスト管理者としてログインしました。"
+  end
+end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
