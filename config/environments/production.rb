3947999859df9271850ddd894ffb29d1fd594223
tommyoguro <tomoharu.oguro.2022@globis.ac.jp>
2025-02-14 11:35:10 +0900

    Add graduateapp.herokuapp.com to config.hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+   config.hosts << "graduateapp.herokuapp.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -49,7 +50,7 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = false # true 
+  config.force_ssl = true 
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
