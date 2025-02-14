26f0eaaff2636be0f4312cba52f622ccccb3bc20
tommyoguro <tomoharu.oguro.2022@globis.ac.jp>
2025-02-14 10:35:48 +0900

    init

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-        def placeholder_field(f, field, type = :text_field)
-          placeholder_text = f.object.class.human_attribute_name(field)
-          f.send(type, field, placeholder: "#{placeholder_text} を入力してください")
-        end
-      end
+  def placeholder_field(f, field, type = :text_field)
+    placeholder_text = f.object.class.human_attribute_name(field)
+    f.send(type, field, placeholder: "#{placeholder_text} を入力してください")
+  end
+end
